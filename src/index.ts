e16e048153abfd3b97fc18151aee8f7434fb0f9f
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-13 22:12:49 +0530

    Project Initialization 2

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,8 @@ const wss = new WebSocketServer({port: 8080});
 let userCount = 0;
 
 wss.on("connection", (socket) => {
-    console.log("User COnnected");
+    console.log("User Connected");
+    userCount++;
+    console.log("User COnnected #" + userCount);
+
 })
