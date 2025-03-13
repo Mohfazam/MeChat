a0194b4ce40bbfcf55c21b7a315a21f865f3ee41
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-14 01:24:22 +0530

    Chat Application Backend

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 let userCount = 0;
+let allSockets = [];
 wss.on("connection", (socket) => {
+    allSockets.push(socket);
     console.log("User Connected");
     userCount++;
     console.log("User COnnected #" + userCount);
     socket.on("message", (message) => {
         console.log("Message Received", message.toString());
+        for (let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s.send(message.toString() + ": sent from the server");
+        }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({port: 8080});
 
 let userCount = 0;
+let allSockets:WebSocket[] = [];
 
 wss.on("connection", (socket) => {
+
+    allSockets.push(socket);
+
     console.log("User Connected");
     userCount++;
     console.log("User COnnected #" + userCount);
 
     socket.on("message", (message) => {
         console.log("Message Received", message.toString());
+        for(let i = 0; i < allSockets.length; i++){
+            const s = allSockets[i];
+        s.send(message.toString()+": sent from the server");
+        }
     });
 
 })
