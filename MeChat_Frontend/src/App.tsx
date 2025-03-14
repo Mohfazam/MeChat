bff8f02322e59b67f7d4ea49ad7fb978d74f1d26
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-14 16:08:05 +0530

    Frontend 50% done

--- MeChat_Frontend/src/App.tsx
+++ MeChat_Frontend/src/App.tsx
@@ -1,11 +1,23 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 function App() {
 
+  const  [messages, setMessages] = useState(['hi there']);
+
+  useEffect(() => {
+    const ws = new WebSocket("http://localhost:3000");
+    ws.onmessage = (event) => {
+      setMessages(m => [...m, event.data])
+    }
+  }, [])
 
   return (
     <div className='h-screen bg-black'>
-     <div className='h-[95vh]'></div>
+      <br />
+     <div className='h-[95vh]'>
+     {messages.map(messages => <span className='bg-white text-black rounded p-4 m-4'>{messages}</span>)}
+     </div>
      <div className='w-full bg-white flex p-4'>
         <input type="text" className='flex-1'></input>
         <button className='bg-purple-600 text-white'>Send Message</button>
